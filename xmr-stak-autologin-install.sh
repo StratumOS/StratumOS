4de3a4fffbf018c4e4a7eec3f82bf3f0752deb8f
System 66 <info@system66.uk>
2019-02-22 22:31:21 +0000

    Create xmr-stak-autologin-install.sh

---
+++ xmr-stak-autologin-install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# checking and installing dependencies
+# install screen strace
+apt -y install screen strace
+
+# create stratumos folder
+cd /
+mkdir stratumos
+mkdir stratumos/xmr-stak
+mkdir stratumos/xmr-stak/backup
+cp -a /stratumos/xmr-stak/. /stratumos/xmr-stak/backup/
+cp -a /home/$stratumos_user/xmr-stak/build/bin/. /stratumos/xmr-stak/
+
+cat > /stratumos/xmr-stak/xmr-stak.sh << EOF
+#!/bin/bash
+/usr/bin/screen -dmS xmr-stak '/stratumos/xmr-stak/xmr-stak' --config /stratumos/xmr-stak/config.txt --poolconf /stratumos/xmr-stak/pools.txt &
+EOF
+chmod +x /stratumos/xmr-stak/xmr-stak.sh
+
+cat > /root/.profile  << 'EOF'
+[[ $(tty) = /dev/tty1 ]] && /stratumos/xmr-stak/xmr-stak.sh &
+EOF
+
+# create vi file in sudoers.d folder
+cat > /etc/sudoers.d/$stratumos_user << EOF
+# user $stratumos_user use sudo without password
+$stratumos_user ALL=(ALL) NOPASSWD:ALL
+EOF
+chmod 444 /etc/sudoers.d/$stratumos_user
+
+# use root after login
+printf '%s\n%s\n' 'sudo -s' >> /home/$stratumos_user/.profile
+
+rm /etc/init.d/xmr-stak
+update-rc.d xmr-stak disable
+echo "vm.nr_hugepages=128" >> /etc/sysctl.conf
+sysctl -p
+
+# autologin as root in tty1
+mkdir -pv /etc/systemd/system/getty@tty1.service.d
+rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $USER --noclear %I $TERM
+EOF
+
